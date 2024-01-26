3403753f8f143384c28ed84a34b270df9fd8f32e
kapil619 <kapilbadokar321@gmail.com>
2024-01-26 11:22:32 +0530

    Fixed Can't read of type null error

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -9,14 +9,14 @@ import { usePathname, useRouter } from "next/navigation";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const [copied, setCopied] = useState("");
-  const {data:session} = useSession();
+  const { data: session } = useSession();
   const pathName = usePathname();
   const router = useRouter();
 
   const handleCopy = () => {
     setCopied(post.prompt);
     navigator.clipboard.writeText(post.prompt);
-    setTimeout(() => 
+    setTimeout(() =>
       setCopied(''), 3000);
   };
 
@@ -25,7 +25,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       <div className="flex justify-between items-start-gap-5">
         <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
           <Image
-            src={post.creator.image}
+            src={post.creator?.image}
             alt="user_image"
             width={40}
             height={40}
@@ -33,10 +33,10 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
           />
           <div className="flex flex-col">
             <h3 className="font-satoshi font-semibold text-gray-900">
-              {post.creator.username}
+              {post.creator?.username}
             </h3>
             <p className="font-inter text-sm text-gray-500">
-              {post.creator.email}
+              {post.creator?.email}
             </p>
           </div>
         </div>
@@ -61,7 +61,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         #{post.tag}
       </p>
 
-      {session?.user.id ===post.creator._id && pathName === '/profile' && (
+      {session?.user.id === post.creator?._id && pathName === '/profile' && (
         <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
           <p className="font-inter text-sm green_gradient cursor-pointer" onClick={handleEdit}>Edit
 
